ded5fd397b413f04d7ac45784feb934a732eaf05
pennkao <pennkao@163.com>
2020-04-02 01:49:11 +0800

    crc64

---
+++ exthash/crc64.go
@@ -0,0 +1,10 @@
+package exthash
+
+import "hash/crc64"
+
+func Crc64(str string) uint64{
+	//先建立一個table
+	table := crc64.MakeTable(crc64.ECMA)
+	//傳入位元組切片和table，返回一個uint64
+	return crc64.Checksum([]byte(str), table))
+}
